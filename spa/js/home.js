4edee5eba398a1ac743a540c8ac9c26ad3fe6cf9
Keegan Pereira <pereirak@coventry.ac.uk>
2021-11-25 00:16:23 +0000

    switch home data based on if logged in or not

--- spa/js/home.js
+++ spa/js/home.js
@@ -21,93 +21,51 @@ export async function setup(node) {
   }
 }
 
-/*
-async function addContent(node){
-	const url = 'https://jsonplaceholder.typicode.com/comments'
-	const rsp = await fetch(url)
-	//console.log(rsp)
-	const comments = await rsp.json()
-	console.log(comments)
-	const template = document.querySelector('template#comments')
-	for(const comment of comments){		
-		const fragment = template.content.cloneNode(true)
-		fragment.querySelector('h2').innerText = comment.email
-		fragment.querySelector('p').innerText = comment.body
-		node.appendChild(fragment)
-	}
-}
-*/
-
-
 async function addSurvey(node){
 	console.log('SHOWING SURVEYS')
-	const url = "/api/v1/surveys"
-	const options = {
-		method: "GET",
-		headers: {
-			"Content-Type":"application/vnd.api+json",
-			"Authorization": localStorage.getItem("authorization"),
+	const uri = "/api/v1/surveys"
+	//console.log(uri, JSON.stringify(options, null, 2))
+	
+	//IF AUTHORIZED
+	if(localStorage.getItem("authorization")){
+		const options = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/vnd.api+json",
+				"Authorization": localStorage.getItem("authorization"),
+			},
 		}
+		const rsp = await fetch(uri, options)
+		const surveyObj = await rsp.json()
+		console.log(surveyObj)
+		popSurveyFrags(node, surveyObj)
 	}
-	console.log(url, JSON.stringify(options, null, 2))
-	const rsp = await fetch(url)
-	const surveyObj = await rsp.json()
-	console.log(surveyObj)
+	
+	//IF UNAUTHORIZED
+	if(!localStorage.getItem("authorization")){
+		const options = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/vnd.api+json",
+			},
+		}
+		const rsp = await fetch(uri, options)
+		const surveyObj = await rsp.json()
+		console.log(surveyObj)
+		popSurveyFrags(node, surveyObj)
+	}
+}
+
+function popSurveyFrags(node, obj){
 	const template = document.querySelector('template#surveys')
-	for(const survey of surveyObj.data){		
+	for(const survey of obj.data){
 		const fragment = template.content.cloneNode(true)
 		fragment.querySelector('h2').innerText = survey.name
 		fragment.querySelector('p').innerText = survey.description
 		fragment.querySelector('time').innerText = survey.created
 		fragment.querySelector('b').innerText = survey.questions
-		fragment.querySelector('a').innerText = survey.href
+		if(survey.href){ fragment.querySelector('a').innerText = survey.href
+		}else fragment.querySelector('a').innerText = survey.avgScore
 		node.appendChild(fragment)
 	}
 }
-
-
-
-/*
-async function addSurvey(node){
-	console.log('GETTING SURVEYS')
-	const url = "/api/v1/surveys"
-	const options = {
-		method: "GET",
-		headers: {
-			"Content-Type":"application/vnd.api+json",
-			"Authorization": localStorage.getItem("authorization"),
-		}
-	}
-	console.log(url, options)
-	const rsp = await fetch(url, options)
-	const json = await rsp.json();
-	console.log(json)
-	const template = document.querySelector('template#surveys')
-	for(const survey of json.data){		
-		const fragment = template.content.cloneNode(true)
-		fragment.querySelector('h2').innerText = survey.name
-		fragment.querySelector('p').innerText = survey.description
-		fragment.querySelector('time').innerText = survey.created
-		fragment.querySelector('b').innerText = survey.questions
-		fragment.querySelector('a').innerText = survey.href
-		node.appendChild(fragment)
-	}
-}*/
-
-
-
-/*
-// this example loads the data from a JSON file stored in the uploads directory
-async function addContent(node) {
-	const response = await fetch('/uploads/quotes.json')
-	const quotes = await response.json()
-	const template = document.querySelector('template#quote')
-	for(const quote of quotes.data) {
-		const fragment = template.content.cloneNode(true)
-		fragment.querySelector('h2').innerText = quote.author
-		fragment.querySelector('p').innerText = quote.quote
-		node.appendChild(fragment)
-	}
-	//const node = template.content.cloneNode(true) // get a copy of the template node
-}
-*/
