3e5cca6728445e8c31de42a1278d34151678b1b0
Keegan Pereira <pereirak@coventry.ac.uk>
2021-11-27 22:21:06 +0000

    implemented dynamic question title & description creatation & deletion

---
+++ spa/js/addQuestions.js
@@ -0,0 +1,132 @@
+import {
+  customiseNavbar,
+  file2DataURI,
+  loadPage,
+  secureGet,
+  showMessage,
+} from "../util.js";
+
+export async function setup(node) {
+  console.log("surveyQuetions: setup");
+  try {
+    console.log(node);
+    document.querySelector("header p").innerText = "Survey Questions";
+    customiseNavbar(["home", "logout", "newsurvey"]);
+    if (localStorage.getItem("authorization") === null) loadPage("login");
+    // there is a token in localstorage
+    createForm(node)
+	  
+    await addQuestions(node)
+
+
+    const questions = []
+    const descriptions = []
+    
+    node.append(document.createElement('article'))
+    node.append(document.createElement('main'))
+	  
+    //Handle dynamic question title creation and deletion.	  
+    node.querySelector('input').addEventListener('keyup', (event)=>{
+	    const template = document.querySelector('template#surveyQuestions')
+	    const fragment = template.content.cloneNode(true)
+	    if(event.key === 'Enter') {
+	    const name = event.target.value
+	    const li = document.createElement('li')
+	    li.innerText = event.target.value
+	    console.log("ADDING: ", event.target.value)
+            questions.push(event.target.value)
+	    li.addEventListener('click', (event)=>{
+		    const index = [...event.target.parentNode.children]
+		    .indexOf(event.target)
+		    const deleted = document.querySelector('article').removeChild(li)
+		    console.log("DELETING: ", deleted.innerText)
+		    
+		    const qIdx = questions.indexOf(deleted.innerText)
+		    questions.splice(qIdx, 1);
+		    console.log(questions)
+		    //console.log(`item at index: ${index}`)
+		    //document.querySelector('article').removeChild(li)
+	    })
+	    //const ol = document.createElement('ol')
+	    event.target.value = ''
+	    event.target.select()
+	    document.querySelector('article').appendChild(li)
+	    console.log(questions)
+	  }
+    })
+    
+	  
+	  
+    //Handle dynamic question description creation and deletion.	  
+    node.querySelector('textarea').addEventListener('keyup', (event)=>{
+	    const template = document.querySelector('template#surveyQuestions')
+	    const fragment = template.content.cloneNode(true)
+	    if(event.key === 'Enter') {
+	    const section = document.createElement('section')
+	    section.innerText = event.target.value
+	    const str = event.target.value.replace('\n', '')
+	    console.log("ADDING: ", str)
+            descriptions.push(str)
+	    section.addEventListener('click', (event)=>{
+		    const index = [...event.target.parentNode.children]
+		    .indexOf(event.target)
+		    const deleted = document.querySelector('main').removeChild(section)
+		    console.log("DELETING: ", deleted.innerText)
+		    
+		    const dscIdx = descriptions.indexOf(deleted.innerText)
+		    descriptions.splice(dscIdx, 1);
+		    console.log(descriptions)
+		    //console.log(`item at index: ${index}`)
+		    //document.querySelector('article').removeChild(li)
+	    })
+	    //const ol = document.createElement('ol')
+	    event.target.value = ''
+	    event.target.select()
+	    document.querySelector('main').appendChild(section)
+	    console.log(descriptions)
+	  }
+    })
+	  
+	  
+	  	  
+    //node.querySelector("form").addEventListener("submit", await respond);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function createForm(node){
+	const template = document.querySelector('template#addQuestions')
+	const fragment = template.content.cloneNode(true)
+	//const form = document.createElement('form')
+	const qstLabel = document.createElement('label')
+	qstLabel.innerText = 'Question Title'
+	const question = document.createElement('input')
+	const dscrLabel = document.createElement('label')
+	dscrLabel.innerText = 'Question Description'
+	const description = document.createElement('textarea')
+	
+	qstLabel.appendChild(document.createElement('br'))
+	
+	fragment.appendChild(qstLabel)
+	fragment.appendChild(question)
+	
+	fragment.appendChild(document.createElement('br'))
+	dscrLabel.appendChild(document.createElement('br'))
+	
+	fragment.appendChild(dscrLabel)
+	fragment.appendChild(description)
+
+	node.appendChild(fragment)
+}
+
+
+async function addQuestions(node){
+	console.log('ADDING QUESTIONS')
+	const uriParams = new URLSearchParams(window.location.search)
+	const surveyid = uriParams.get('mysurvey')
+	console.log(surveyid)
+}
+
+
+
--- spa/js/mySurveys.js
+++ spa/js/mySurveys.js
@@ -28,4 +28,45 @@ export async function setup(node) {
 
 async function getMySurveys(node){
 	console.log("GETTING MY SURVEYS")
+	const uri = "/api/v1/mysurveys"
+	const options = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/vnd.api+json",
+			"Authorization": localStorage.getItem("authorization"),
+		},
+	}
+	const rsp = await fetch(uri, options)
+	const surveyObj = await rsp.json()
+	console.log(surveyObj)
+	
+	
+	
+	//create dom
+	const template = document.querySelector('template#mySurveys')
+	const fragment = template.content.cloneNode(true)
+	for(const survey of surveyObj.data){
+		const section = document.createElement('section')
+		
+		const name = document.createElement('h2')
+		const created = document.createElement('time')
+		const questions = document.createElement('p')
+		const link = document.createElement('a')
+		link.innerText = 'ADD QUESTIONS'
+		
+		name.innerText = survey.name
+		created.innerText = `Date Created: ${survey.created}`
+		questions.innerText = `Questions: ${survey.questions}`
+		link.href = `/addQuestions?mysurvey=${survey.id}`
+		
+		section.appendChild(document.createElement('hr'))
+		section.appendChild(name)
+		section.appendChild(created)
+		section.appendChild(questions)
+		section.appendChild(link)
+		
+		fragment.appendChild(section)
+		//fragment.appendChild(document.createElement('hr'))
+	}
+	node.appendChild(fragment)
 }
